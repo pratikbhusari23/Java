a681bc6038e64ffdb80c4f5f572d46a04333682b
Pratik Bhusari <pratikbhusari77@gmail.com>
2023-03-16 23:59:00 +0530

    new codes added

---
+++ Assignments/Assignment-0/program1.java
@@ -0,0 +1,16 @@
+class Evenodd{
+	public static void main(String[] pratik){
+	
+		
+		int x=10;
+
+		if(x%2==0){
+		
+			System.out.println("Even");
+
+		}
+		else {
+			System.out.println("Odd");
+		}
+	}
+}
---
+++ Assignments/Assignment-0/program2.java
@@ -0,0 +1,24 @@
+/*
+ *
+ Write a java program , take a no & print whether it is less than 10 or greater than 10
+
+ */
+
+class Greatercheck{
+	
+	public static void main(String[] args){
+		
+		int x=11;
+
+		if(x>10){
+			
+			System.out.println(x +" is greater than 10");
+		}
+		else if(x==10){
+			System.out.println(x +" is equal to 10");
+		}
+		else {
+			System.out.println(x +"is lesser than 10");
+		}
+	}
+}
