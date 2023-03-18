bc16cda3e8bc61d5a214a53cd5cce9a408c3afdd
Pratik Bhusari <pratikbhusari77@gmail.com>
2023-03-18 21:37:39 +0530

    new codes added

---
+++ Quiz/Forloop-Basic/program8.java
@@ -0,0 +1,59 @@
+	
+class Core2web{
+	
+	public static void main(String[] pratik){
+		
+	//	int x=10;
+/*
+		{
+			int x=20;
+			System.out.println(x);
+		}
+		{
+			int x=30;
+			System.out.println(x);
+		}
+		{
+			System.out.println(x);
+		}
+	}
+	
+	
+		int x=10;
+
+		{
+			int y=20;
+			System.out.println(x+" "+y);
+		}
+		{
+			int y=15;
+			System.out.println(x+" "+y);
+		}
+
+			System.out.println(x+" "+y );
+	}
+	*/
+/*
+	int x=10;
+	{
+		int y=20;
+		System.out.println(x +" "+y);
+	}
+	{
+		System.out.println(x+" "+y);
+	}
+	System.out.println(x+" "+y);
+	}*/
+
+	int x=10,y=20;
+
+	{
+		System.out.println(x+ " " + y);
+	}
+	{
+		x=15;
+		System.out.println(x+" "+y);
+	}
+	System.out.println(x+" "+y);
+	}
+}
