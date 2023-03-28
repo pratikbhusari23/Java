4fdec16a2250e26fa0f352b5744b5a705cd7fbf2
Pratik Bhusari <pratikbhusari77@gmail.com>
2023-03-28 22:41:58 +0530

    switch codes added

---
+++ Class-Codes/Switch/program1.java
@@ -0,0 +1,32 @@
+class Switch{
+	public static void main(String[] pratik){
+	
+		int x=3;
+
+		switch(x){
+			
+			case 1 :
+				System.out.println("One");
+			case 2 :
+				System.out.println("Two");
+			case 3 :
+				System.out.println("Three");
+			case 4 :
+				System.out.println("Four");
+			default :
+				System.out.println("No match");
+		}
+		System.out.println("After Switch");
+	}
+	
+}	
+/*
+
+
+Three
+Four
+No match
+After Switch
+
+
+*/
---
+++ Class-Codes/Switch/program6.java
@@ -0,0 +1,24 @@
+class Switch{
+	public static void main(String[] pratik){
+		
+		String Str="Mon";
+
+		switch(Str){
+		
+			case "Mon":
+				System.out.print("Monday\n");
+				break;
+			case "Tue":
+				System.out.print("Tuesday");
+				break;
+			default :
+				System.out.print("Sunday");
+				
+
+		}
+		
+
+	}
+	
+}	
+
