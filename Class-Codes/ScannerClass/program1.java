8ce7f30425df679bafad02cca892ebdaee3c531d
Pratik Bhusari <pratikbhusari77@gmail.com>
2023-04-01 22:01:05 +0530

    new codes added

---
+++ Class-Codes/ScannerClass/program1.java
@@ -0,0 +1,12 @@
+class program1{
+	
+	public static void main(String args[]) throws Exception{
+		
+		System.out.println("Enter a number");
+		float num =System.in.read();
+
+		System.out.println(num);
+	
+	}
+
+}
