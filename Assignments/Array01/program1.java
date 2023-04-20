4b60d7d5e26293a67d52e722d8c750077e5890e5
Pratik Bhusari <pratikbhusari77@gmail.com>
2023-04-20 23:56:55 +0530

    Array Codes added

---
+++ Assignments/Array01/program1.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+
+class Demo {
+
+	public static void main(String[] args)throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter Size of Array :");
+		int size = Integer.parseInt(br.readLine());
+
+		int arr[]= new int[size];
+		
+		int sum=0;
+		
+		System.out.println("Enter Array Elements : ");
+		for(int i=0 ; i<arr.length ; i++){
+			
+			arr[i]=Integer.parseInt(br.readLine());
+
+			if(arr[i]%2!=0){
+				
+				sum+=arr[i];
+			}
+		}
+
+		System.out.println("Sum of Odd elements : "+sum);
+
+
+
+	}
+}
---
+++ Assignments/Array01/program2.java
@@ -0,0 +1,41 @@
+
+import java.io.*;
+
+class Demo {
+
+	public static void main(String[] args)throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter Size of Array :");
+		int size = Integer.parseInt(br.readLine());
+
+		int arr[]= new int[size];
+		
+		int product=1;
+		
+		System.out.println("Enter Array Elements : ");
+		
+		int flag=0; //if Zero even elements Present
+
+		for(int i=0 ; i<arr.length ; i++){
+			
+			arr[i]=Integer.parseInt(br.readLine());
+
+			if(arr[i]%2==0){
+				flag=1;  // If even elements present
+				product*=arr[i];
+			}
+		}
+		
+		if(flag==0){
+		
+			System.out.println("No Even elements Present");
+		}
+		else{
+			System.out.println("Sum of Odd elements : "+product);
+		}
+
+
+	}
+}
