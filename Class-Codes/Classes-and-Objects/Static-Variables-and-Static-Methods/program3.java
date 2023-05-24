de52b6c0291cc9f675efb913b4c0e91930427195
Pratik Bhusari <pratikbhusari77@gmail.com>
2023-05-24 23:55:29 +0530

    new codes added

--- Class-Codes/Classes-and-Objects/Static-Variables-and-Static-Methods/program3.java
+++ Class-Codes/Classes-and-Objects/Static-Variables-and-Static-Methods/program3.java
@@ -1,16 +1,37 @@
 class Demo{
 	
 	int x=10;
+
 	static{
+
 		Demo obj = new Demo();
 
 		System.out.println("Static Block");
 	
 		System.out.println(obj.x);
+		int y=20;
+		System.out.println(y);
+	
+	}
+	
+	static {
+		fun();
+	}
+	
+	static void fun(){
+		
+		System.out.println(2+3);
+	
 	}
 	
 	public static void main(String[] pratik){
 		
 		System.out.println("Main Method");
+	
+	}
+
+	static {
+		fun();
+
 	}
 }
---
+++ Class-Codes/Classes-and-Objects/Static-Variables-and-Static-Methods/program5.java
@@ -0,0 +1,13 @@
+class Demo{
+	
+
+	public static void main(String[] pratik){
+		
+		
+		System.out.println("In Main");
+	}
+	static {
+		
+		System.out.println("In static block");
+	}
+}
