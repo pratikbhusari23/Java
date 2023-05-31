e1c0027762f151601abd1c9c6ac26849f8c60617
pratikbhusari23 <pratikbhusari77@gmail.com>
2023-05-31 23:59:00 +0530

    Inheritance codes added

---
+++ Class-Codes/Inheritance/program1.java
@@ -0,0 +1,30 @@
+class Parent{
+	
+	Parent(){
+		System.out.println("In Parent Constructor");
+	}
+
+	void ParentProperty(){
+		
+		System.out.println("Flat,Gold,Car");
+	}
+
+}
+
+class Child extends Parent{
+	
+	Child(){
+		
+		System.out.println("In Child Constructor");
+	}
+}
+
+class Client{
+	
+	public static void  main(String[] args){
+		
+		Child obj1 = new Child();
+
+		obj1.ParentProperty();
+	}
+}
