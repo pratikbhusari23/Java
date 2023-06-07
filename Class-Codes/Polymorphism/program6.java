cb66a7edc0c8b843db256d340bc1e9b19d6fca33
pratikbhusari23 <pratikbhusari77@gmail.com>
2023-06-08 00:00:05 +0530

    new code added

---
+++ Class-Codes/Polymorphism/program6.java
@@ -0,0 +1,32 @@
+class Parent{
+	
+	Parent(){
+		System.out.println("Parent Constructor");
+	}
+
+	void fun(){
+		System.out.println("In fun");
+	}
+}
+class Child extends Parent{
+
+	Child(){
+		System.out.println("Child Constructor");
+	}
+
+	void gun(){
+		System.out.println("In gun");
+	}
+}
+
+class Client{
+	public static void main(String[] args){
+		
+		Child obj1 = new Child();
+
+		obj1.fun();
+		obj1.gun();
+
+		Parent obj2 = new Parent();
+	}
+}
