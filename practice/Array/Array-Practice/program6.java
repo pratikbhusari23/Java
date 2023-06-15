99d68ee9fae78a5fc59dfb2360eb274f26fadddf
pratikbhusari23 <pratikbhusari77@gmail.com>
2023-06-15 23:53:07 +0530

    Hashmap codes added

---
+++ practice/Array/Array-Practice/program6.java
@@ -0,0 +1,33 @@
+/*
+
+Write a program to create an array of ‘n’ integer elements.
+Where ‘n’ value should be taken from the user.
+Insert the values from users and find the sum of all elements in the array.
+Input:
+n=6
+Enter elements in the array:
+2
+3
+6
+9
+5
+1
+Output:
+26
+
+ */
+
+import java.io.*;
+
+class Program6{
+	
+	public static void main(String[] args)throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Enter size of array");
+		int size = Integer.parseInt(br.readLine());
+
+
+	}
+}
---
+++ practice/HashMap/program1.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Demo{
+	
+	public static void main(String[] args){
+		
+		// CREATING HASHMAP
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		// INSERTION IN HASHMAP
+	
+		map.put("China",150);
+		map.put("India",120);
+		map.put("USA",100);
+		
+		// PRINTING HASHMAP
+		
+		System.out.println(map);	// {USA=100, China=150, India=120}
+
+		map.put("China",180);		
+		System.out.println(map);	// {USA=100, China=180, India=120}	
+
+		// SEARCHING
+		
+		if(map.containsKey("Indonesia")){
+			
+			System.out.println("Present");
+		
+		}
+		else{
+			
+			System.out.println("Not Present");	// Not Present
+		
+		}
+
+		System.out.println(map.get("China"));		//180
+		System.out.println(map.get("Indonesia"));	//Null
+
+		// Iteration Type-1
+		
+		for(Map.Entry<String , Integer> e : map.entrySet()){
+
+			System.out.println(e.getKey());
+			System.out.println(e.getValue());
+		}
+
+		// Iteration Type-2
+
+		Set<String> keys = map.keySet();
+
+		for(String key : keys){
+			
+			System.out.println(key+" "+map.get(key));
+		}
+
+		// Remove
+		
+		map.remove("China");
+		System.out.println(map);
+	}
+}
