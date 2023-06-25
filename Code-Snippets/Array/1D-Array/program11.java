649b67ef971f0f1f02fdf940f1894ad9723d9b8c
pratikbhusari23 <pratikbhusari77@gmail.com>
2023-06-25 22:07:56 +0530

    one coded added

---
+++ Code-Snippets/Array/1D-Array/program11.java
@@ -0,0 +1,12 @@
+class Core2web{
+	
+	public static void main(String[] args){
+
+		double[] arr = {12d , 13d , 14d , 15d};
+
+		for(double var : arr){
+			
+			System.out.println(arr.length);
+		}
+	}
+}
