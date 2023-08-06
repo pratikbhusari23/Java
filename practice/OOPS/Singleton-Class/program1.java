1529566660fa67ad81b40dd4315f377894ffc24a
pratikbhusari23 <pratikbhusari77@gmail.com>
2023-08-06 23:52:19 +0530

    new code added

---
+++ practice/OOPS/Singleton-Class/program1.java
@@ -0,0 +1,26 @@
+class Demo{
+		
+		
+		private static Demo obj = null;
+
+		private Demo(){}
+
+		public static Demo getTest(){
+			
+			if(obj==null){
+				
+				obj=new Demo();
+			}
+			return obj;
+		}
+	
+}
+
+class Client{
+	
+	public static void main(String[] args){
+		
+		System.out.println(Demo.getTest().hashCode());
+		System.out.println(Demo.getTest().hashCode());
+	}
+}
