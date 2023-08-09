e1fea9116c337fedad2e17bf487c1453781614b6
pratikbhusari23 <pratikbhusari77@gmail.com>
2023-08-09 22:03:47 +0530

    new codes added

---
+++ practice/OOPS/nptel/pr2.java
@@ -0,0 +1,8 @@
+class Demo{
+	public static void main(String[] args){
+		
+		int a[] = new int[0];
+		a[0]=12;
+		System.out.println(a);
+	}
+}
---
+++ practice/OOPS/nptel/pr3.java
@@ -0,0 +1,8 @@
+class Main{
+	
+	public static void main(String[] args){
+		
+		int [] a= new int[20];
+		System.out.println(a[10]);
+	}
+}
