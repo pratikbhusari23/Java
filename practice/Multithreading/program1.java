77e0654d6da5d73301e7320af53051664939ee05
pratikbhusari23 <pratikbhusari77@gmail.com>
2023-08-22 23:36:07 +0530

    new codes added

---
+++ practice/Multithreading/program1.java
@@ -0,0 +1,23 @@
+class Mythread extends Thread{
+	public void run(){
+		System.out.println("This line is executed by thread : "+Thread.currentThread().getName());
+	}
+}
+
+class Demo{
+	
+	public static void main(String[]args){
+	
+		System.out.println(Thread.currentThread().getName());
+		Mythread t = new Mythread();
+		t.start();
+		Mythread t1=new Mythread();
+		t1.start();
+		System.out.println(t.getName());
+		System.out.println(t1.getName());
+		
+		Thread.currentThread().setName("pratik");
+		System.out.println(Thread.currentThread().getName());		
+	
+	}
+}
